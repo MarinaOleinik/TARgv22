64d83c1261fe592d90a0ffa4667b37e46d241c20
marina.oleinik <marina.oleinik@HP-CZC6297B9D>
2022-11-28 17:24:48 +0200

    Korduslaused

--- TARgv22/TARgv22.py
+++ TARgv22/TARgv22.py
@@ -1,8 +1,87 @@
-
 from math import *
+j=0
+for i in range(0,15,1): # for i in range(15):
+    A=float(input(f"{i+1} Sisesta A  : ")) #A=5.0 int(A)==A =True
+    if int(A)==A: j+=1
+print(j)
+j=0
+i=0
+while True:
+    i+=1
+    A=float(input(f"{i} Sisesta A  : "))
+    if int(A)==A: j+=1
+    if i==15: break
+print(j)
+j=0
+i=0
+while i<15:
+    i+=1
+    A=float(input(f"{i} Sisesta A  : "))
+    if int(A)==A: j+=1
+print(j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Tere! Olen sinu uus sõber - Python!")
+a=input("! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
+if a=="1":
+
+    while True:
+        try:
+            pikkus=int(input("Pikkus: "))
+            if pikkus>0 and pikkus<273: break
+        except:
+            print("Error") 
+    mass=-1
+    while mass<0 or mass>400:
+        try:
+            mass=int(input("Mass: "))        
+        except:
+            print("Error")
+
+    try:
+        index=mass/pikkus
+        if index<16:
+            pass
+        elif index>=16 and index<19:
+            pass
+    except:
+        print("Error")
+
+
+else:
+    print("Kahju! See on väga kasulik info!")
+
+
+
+
+
+
+
+
+
+
+
 
 #print("Tere tulemast!!!") #kommenteerime
+
 arv=input("Arv")
+
+
 try:
     arv=int(arv)
     print("Int")  
@@ -11,7 +90,7 @@ except:
         arv=float(arv)
         print("Float")
     except:
-        print("tekst")
+        print("Tekst")
     
 
 
@@ -63,7 +142,7 @@ if vastus.upper()=="JAH":
     tulemus="Suured tähed"
 
 arv=input("Sisesta arv")
-print(arv[:1])
+#print(arv[:1])
 if arv.isalpha():
     print("See on tekst")
 elif arv.isdigit():
